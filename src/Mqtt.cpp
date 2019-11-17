9949f022a2e0a28e94ad0394dec76c1cf088a158
aligator <aligator@suncraft-server.de>
2019-11-17 11:06:30 +0100

    check color input for correct format

--- src/Mqtt.cpp
+++ src/Mqtt.cpp
@@ -47,13 +47,18 @@ void Mqtt::_receiveCallback(char* topic, byte* payload, unsigned int length) {
             int brightness = message.toInt();
             if (brightness >= 0 && brightness <= 255) {
                 led->setBrightness(brightness);
+            } else {
+                Serial.println("the brightness has to be a integer from 0 to 255");
             }
         }
     } else if (strcmp(topic, TOPIC_SET_FPS) == 0) {
+        // -1 is fps disabled -> as fast as it can
         if (Mqtt::_isValidNumber(message)) {
             int fps = message.toInt();
             if (fps >= -1 && fps <= 500) {
                 led->setFps(fps);
+            } else {
+                Serial.println("the fps has to be a integer from -1 to 500");
             }
         }
     } else if (strcmp(topic, TOPIC_SET_TEXT_COLOR) == 0) {
@@ -65,17 +70,38 @@ void Mqtt::_receiveCallback(char* topic, byte* payload, unsigned int length) {
 
         std::vector<uint16_t> colors = std::vector<uint16_t>();
 
-        JsonArray colorArray=doc.as<JsonArray>();
+        if (doc.is<JsonArray>()) {
+            JsonArray colorArray=doc.as<JsonArray>();
 
-        for (JsonArray color : colorArray) {
-            int r = color[0];
-            int g = color[1];
-            int b = color[2];
-            
-            colors.push_back(led->matrix.Color(r, g, b));
+            for (JsonVariant colorVariant : colorArray) {
+                if (!colorVariant.is<JsonArray>()) {
+                    Serial.println("the colors have to be arrays of 3 values (r, g, b)");
+                    return;
+                }
+
+                JsonArray color = colorVariant.as<JsonArray>();
+                if (color.size() == 3) {
+                    if(color[0].is<short>() && (short)color[0] >= 0 && (short)color[0] <= 255 &&
+                        color[1].is<short>() && (short)color[1] >= 0 && (short)color[1] <= 255 &&
+                        color[2].is<short>() && (short)color[2] >= 0 && (short)color[2] <= 255) {
+
+                        short r = color[0];
+                        short g = color[1];
+                        short b = color[2];
+                        
+                        colors.push_back(led->matrix.Color(r, g, b));
+                    } else {
+                        Serial.println("the color values have to be integers from 0 to 255");
+                    }
+                } else {
+                    Serial.println("the colors have to be arrays of 3 values (r, g, b)");
+                }
+            }
+
+            textModule.setColors(colors);
+        } else {
+            Serial.println("you have to pass an array");
         }
-
-        textModule.setColors(colors);
     }
 }
 
